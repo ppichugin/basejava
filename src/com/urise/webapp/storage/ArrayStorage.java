955310382531e9115ec888db965a8c769113623a
Petr Pichugin <petr.pichugin@gmail.com>
2022-01-31 18:32:55 +0500

    Methods optimized

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -21,12 +21,11 @@ public class ArrayStorage {
     }
 
     public void update(Resume resume) {
-        // получаем ссылку на ячейку массива
-        Resume resumeToBeUpdated = get(resume.getUuid());
         // если такое резюме есть в базе, то обновляем его
-        if (resumeToBeUpdated != null) {
-            resumeToBeUpdated = resume;
-            System.out.println("ОК UPDATE. Резюме '" + resumeToBeUpdated.getUuid() + "' обновлено.");
+        int indexOfResumeToBeUpdated = searchResume(resume.getUuid());
+        if (indexOfResumeToBeUpdated >= 0) {
+            storage[indexOfResumeToBeUpdated] = resume;
+            System.out.println("ОК UPDATE. Резюме '" + storage[indexOfResumeToBeUpdated].getUuid() + "' обновлено.");
         } else {
             System.out.println("ERROR UPDATE: Резюме '" + resume.getUuid() + "' в базе нет.");
         }
@@ -37,10 +36,11 @@ public class ArrayStorage {
             System.out.println("Вы не ввели uuid. Повторите ввод команды в формате: save <uuid>");
             return;
         }
+        int indexOfFoundResume = searchResume(r.getUuid());
         if (size == storage.length) {
             System.out.println("ERROR. Сохранение невозможно. База резюме заполнена.");
             // если введенный uuid уникален, то сохраняем резюме в базу
-        } else if (get(r.getUuid()) == null) {
+        } else if (indexOfFoundResume < 0) {
             storage[size] = r;
             size++;
             System.out.println("OK SAVE. Резюме '" + r.getUuid() + "' сохраненно.");
@@ -50,10 +50,12 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].getUuid())) {
-                return storage[i];
-            }
+        int indexOfResumeToBeFound = searchResume(uuid);
+        try {
+            System.out.println("OK GET. Резюме с таким uuid '" + storage[indexOfResumeToBeFound].getUuid() + "' имеется в базе.");
+            return storage[indexOfResumeToBeFound];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR GET. Резюме с таким uuid '" + uuid + "' нет в базе.");
         }
         return null;
     }
@@ -65,40 +67,39 @@ public class ArrayStorage {
             return;
         }
         // удаляем резюме, если есть совпадения uuid
-        for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].getUuid())) {
-                // если это последний элемент в массиве, то удаляем резюме и выходим из метода
-                if (i == storage.length - 1) {
-                    storage[i] = null;
-                } else {
-                    // вар.1 перемещаем ячейки внутри массива со сдвигом влево, если нужно сохранять последовательность
-                    //System.arraycopy(storage, i + 1, storage, i, size - (i + 1));
-                    // вар.2 перемещаем последнюю ячейку на место удаляемой
-                    storage[i] = storage[size - 1];
-                    // обнуляем последнюю ячейку диапазона как дубликат
-                    storage[size - 1] = null;
-                }
-                System.out.println("OK DELETE. Резюме '" + uuid + "' удалено");
-                size--;
-                return;
+        int indexOfResumeToBeDeleted = searchResume(uuid);
+        try {
+            // если это последний элемент в массиве, то удаляем резюме и выходим из метода
+            if (indexOfResumeToBeDeleted == storage.length - 1) {
+                storage[indexOfResumeToBeDeleted] = null;
+            } else {
+                storage[indexOfResumeToBeDeleted] = storage[size - 1];
+                storage[size - 1] = null;
             }
+            System.out.println("OK DELETE. Резюме '" + uuid + "' удалено");
+            size--;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR DELETE. Резюме с таким uuid '" + uuid + "' нет в базе.");
         }
-        System.out.println("ERROR DELETE. Резюме с таким uuid '" + uuid + "' нет в базе.");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] resumes = new Resume[size];
-        // вар.1 (более быстрый?)
-        //System.arraycopy(storage, 0, resumes, 0, size);
-        // вар.2
-        resumes = Arrays.copyOf(storage, size);
-        return resumes;
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
         return size;
+    }
+
+    public int searchResume(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
