aa504eacd6804f6b873c2feb7939a8086889e759
Petr Pichugin <petr.pichugin@gmail.com>
2022-02-03 18:31:16 +0500

    Override equals&hashCode

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -4,7 +4,6 @@ package com.urise.webapp.model;
  * Initial resume class
  */
 public class Resume {
-
     // Unique identifier
     private String uuid;
 
@@ -14,6 +13,19 @@ public class Resume {
 
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
     }
 
     @Override
