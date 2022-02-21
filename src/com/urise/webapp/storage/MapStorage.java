b060b6fcbab5969be7ddad272adac22613ef758f
Petr Pichugin <petr.pichugin@gmail.com>
2022-02-21 18:11:07 +0500

    Lesson05 HW5 Part_2 optimized imports

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -2,7 +2,11 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
 
--- test/com/urise/webapp/storage/MapStorageTest.java
+++ test/com/urise/webapp/storage/MapStorageTest.java
@@ -2,8 +2,6 @@ package com.urise.webapp.storage;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MapStorageTest extends AbstractArrayStorageTest {
 
     public MapStorageTest() {
