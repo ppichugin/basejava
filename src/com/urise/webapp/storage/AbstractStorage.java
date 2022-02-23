a2db3c9a14df8b74bd9cb17e925464322802718b
Petr Pichugin <petr.pichugin@gmail.com>
2022-02-23 14:22:13 +0500

    Lesson05 HW5 printouts moved to interface, renamed two methods

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -10,29 +10,17 @@ import com.urise.webapp.model.Resume;
 public abstract class AbstractStorage implements Storage {
 
     public void clear() {
-        writeConfirmationToConsole("CLEARED", "");
-    }
-
-    private void writeConfirmationToConsole(String status, String uuid) {
-        if (status == null) return;
-        switch (status) {
-            case "CLEARED" -> System.out.println("OK. Storage of resume cleared.");
-            case "UPDATED" -> System.out.println("OK UPDATE. Resume '" + uuid + "' updated.");
-            case "SAVED" -> System.out.println("OK SAVE. Resume '" + uuid + "' saved.");
-            case "EXIST" -> System.out.println("OK GET. Resume '" + uuid + "' exists.");
-            case "DELETED" -> System.out.println("OK DELETE. Resume '" + uuid + "' deleted.");
-            default -> System.out.println();
-        }
+        clearedOk();
     }
 
     public final void update(Resume r) {
         String uuid = r.getUuid();
-        Object searchKey = getKeyForNotExistedResume(uuid);
+        Object searchKey = getKeyForExistedResume(uuid);
         updateResume(r, searchKey);
-        writeConfirmationToConsole("UPDATED", uuid);
+        updatedOk(uuid);
     }
 
-    private Object getKeyForNotExistedResume(String uuid) {
+    private Object getKeyForExistedResume(String uuid) {
         Object searchKey = getSearchKey(uuid);
         if (!isResumeExist(searchKey)) throw new NotExistStorageException(uuid);
         return searchKey;
@@ -44,13 +32,13 @@ public abstract class AbstractStorage implements Storage {
 
     public final void save(Resume r) {
         String uuid = r.getUuid();
-        if (isUuidNull(uuid)) return;
-        Object searchKey = getKeyForExistedResume(uuid);
+        if (isNull(uuid)) return;
+        Object searchKey = getKeyForNotExistedResume(uuid);
         insertResume(r, searchKey);
-        writeConfirmationToConsole("SAVED", uuid);
+        savedOk(uuid);
     }
 
-    private boolean isUuidNull(String uuid) {
+    private boolean isNull(String uuid) {
         if (uuid == null) {
             System.out.println("uuid was not entered. Please try again.");
             return true;
@@ -58,7 +46,7 @@ public abstract class AbstractStorage implements Storage {
         return false;
     }
 
-    private Object getKeyForExistedResume(String uuid) {
+    private Object getKeyForNotExistedResume(String uuid) {
         Object searchKey = getSearchKey(uuid);
         if (isResumeExist(searchKey)) throw new ExistStorageException(uuid);
         return searchKey;
@@ -69,18 +57,18 @@ public abstract class AbstractStorage implements Storage {
     protected abstract void insertResume(Resume r, Object searchKey);
 
     public final Resume get(String uuid) {
-        Object searchKey = getKeyForNotExistedResume(uuid);
-        writeConfirmationToConsole("EXIST", uuid);
+        Object searchKey = getKeyForExistedResume(uuid);
+        exists(uuid);
         return getResume(searchKey);
     }
 
     protected abstract Resume getResume(Object searchKey);
 
     public final void delete(String uuid) {
-        if (isUuidNull(uuid)) return;
-        Object searchKey = getKeyForNotExistedResume(uuid);
+        if (isNull(uuid)) return;
+        Object searchKey = getKeyForExistedResume(uuid);
         removeResume(searchKey);
-        writeConfirmationToConsole("DELETED", uuid);
+        deletedOk(uuid);
     }
 
     protected abstract void removeResume(Object searchKey);
