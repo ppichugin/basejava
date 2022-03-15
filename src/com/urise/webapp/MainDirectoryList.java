1038106030d2b368b5e9cf898c39362d3adc14ec
Petr Pichugin <petr.pichugin@gmail.com>
2022-03-15 12:57:20 +0500

    Lesson09 HW08 recursive directory printout

--- src/com/urise/webapp/MainDirectoryList.java
+++ src/com/urise/webapp/MainDirectoryList.java
@@ -1,40 +1,45 @@
 package com.urise.webapp;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class MainDirectoryList {
-    static List<File> filesAndFolders = new ArrayList<>();
     static int files;
     static int folders;
+    static int count;
 
     public static void main(String[] args) {
         final String pathname = "./src";
-        listFiles(new File(pathname));
-        System.out.println("Files: \n" + filesAndFolders.stream()
-                .sorted(Comparator.comparing(File::getPath))
-                .map(File::getName)
-                .collect(Collectors.joining("\n")));
+        listFilesDeeply(new File(pathname));
         System.out.println("Files: " + files + "\nFolders: " + folders);
     }
 
-    private static void listFiles(File dir) {
-        if (!dir.isDirectory()) return;
+    private static void listFilesDeeply(File dir) {
         File[] list = dir.listFiles();
-        if (list != null) {
-            for (File name : list) {
-                if (name.isDirectory()) {
-                    folders++;
-                    listFiles(name);
-                }
-                if (name.isFile()) {
-                    files++;
-                    filesAndFolders.add(name);
-                }
+        if (list == null) return;
+        Arrays.sort(list, (File o1, File o2) -> {
+            if (o2.isDirectory()) return 1;
+            if (o1.isDirectory()) return -1;
+            return 0;
+        });
+        for (File file : list) {
+            if (file.isDirectory()) {
+                folders++;
+                count++;
+                System.out.println(getSpace(count) + "/" + file.getName());
+                listFilesDeeply(file);
+            }
+            if (file.isFile()) {
+                files++;
+                System.out.println(getSpace(count) + "-" + file.getName());
             }
         }
+        count--;
+    }
+
+    private static StringBuilder getSpace(int length) {
+        StringBuilder str = new StringBuilder();
+        str.append("|").append("-".repeat(Math.max(1, length)));
+        return str;
     }
 }
